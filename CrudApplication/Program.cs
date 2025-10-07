82d4ab0e48e0ac22a54a648efb5cd4bc13b953ca
Elias Araujo <eliasaraujxdev@gmail.com>
2025-10-07 15:07:27 -0300

    Removi as configurações do Docker e alterei o banco de dados para MySQL

--- CrudApplication/Program.cs
+++ CrudApplication/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
